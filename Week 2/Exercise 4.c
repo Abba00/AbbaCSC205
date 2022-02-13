ed781532afaeeb8b8a6ea050862ca50527586780
Abba-ali Concern <aliconcern@gmail.com>
2022-02-13 10:58:07 +0100

    P

--- Week 2/Exercise 4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char myInitial = 'A';
-    char myName[] = "Abba Ali-Concern";
-    printf("Your name is = %s\n", myName);
-    printf("The initial of your firstname is = %c\n", myInitial);
-    return 0;
-}
---
+++ Week 5/Week5_ClassProject1_incomplete.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Class Project 1 incomplete");
+    return 0;
+}
